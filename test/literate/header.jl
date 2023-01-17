daaba4339b6a925f36bbab3a371ab947a4c3b494
Jeff Eldredge <jdeldre@g.ucla.edu>
2023-01-17 12:24:16 -0800

    Updated header

--- test/literate/header.jl
+++ test/literate/header.jl
@@ -1,4 +1,4 @@
 # <img src="https://raw.githubusercontent.com/UCLAMAEThreads/FluidDynTools.jl/main/notebook/logo_UCLA_blue_boxed.svg" alt="ucla logo" width="150" align="left"/>
 
-# ### **Introduction to Fluid Dynamics **
+# ### **Introduction to Fluid Dynamics**
 # <hr />
