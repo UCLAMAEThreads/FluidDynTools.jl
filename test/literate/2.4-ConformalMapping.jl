bfab674d9dd15b8a40532a931d7972569ee3d904
Jeff Eldredge <jdeldre@g.ucla.edu>
2023-03-03 08:14:25 -0800

    Added notebooks 2.4 and 2.5 on conformal mapping

---
+++ test/literate/2.4-ConformalMapping.jl
@@ -0,0 +1,156 @@
+include("header.jl")
+
+#=
+# General potential flows around bodies in 2D
+In the previous notebook, we looked at how we could assemble flows past
+bodies through superposition of singularities. That provided a
+nice way to understand the intuition of the solution. However,
+it turns out that there is a more elegant procedure for constructing
+the solution of potential flow around 2D objects.
+
+The procedure consists of two ingredients
+1. We solve the problem for a body in the shape of the unit circle (a circle of radius 1)
+2. We map that circle geometry into the actual geometry we desire
+
+If the mapping in step 2 is a **conformal transformation**, then the solution
+for the unit circle problem is guaranteed to be the solution for the
+actual problem. This is because conformal transforms preserve a lot of
+the important aspects of the flow, particularly the no-flow-through condition.
+
+In this notebook we will look at some examples of conformal transforms. Then, in
+the next notebook, we will explore flow fields for some basic problems.
+=#
+
+using FluidDynTools
+#-
+using PotentialFlow
+using PotentialFlow.Bodies
+#-
+using Plots
+
+#=
+## Examples of conformal transforms
+A conformal transform has the general form $z(\zeta)$, mapping points from the
+$\zeta$ plane (where the circle lives) to the $z$ plane, where our "real"
+geometry lives. We will make use of a rigid-body transform to place the
+desired shape at the right spot at the right orientation.
+
+$$z(\zeta) = Z_{r} + \mathrm{e}^{\mathrm{i}\alpha} \tilde{z}(\zeta)$$
+
+Thus, the shape itself is specified by the conformal transform $\tilde{z}(\zeta)$,
+and $Z_{r}$ and $\alpha$ specify the position and angle.
+
+We will look at two ways of constructing conformal transforms:
+1. By specifying the coefficients in a power series
+2. By a class of transforms that map a circle into a polygon
+
+### Power series bodies
+First, the power series type. To create a body shape, we specify the complex coefficients $c_{i}$ in the mapping
+
+\begin{equation}
+\tilde{z}(\zeta) = c_{1}\zeta + c_{0} + \sum_{j=1}^{\infty} \frac{c_{-j}}{\zeta^j}
+\end{equation}
+
+The coefficients are specified in the order in which they appear in this series:
+`ccoeff[1]` $\rightarrow c_{1}$, `ccoeff[2]` $\rightarrow c_{0}$, `ccoeff[3]` $\rightarrow c_{-1}$, and so on.
+
+Usually, we do not specify $c_{0}$, because we
+
+An elliptical body just consists of $c_{1}$ and $c_{-1}$, where $c_{1} = (a+b)/2$
+and $c_{-1} = (a-b)/2$, where $a$ and $b$ are the semi-major and minor axis lengths.
+We will place the body at the origin, rotated by $\alpha = \pi/4$. We use
+the [`ConformalBody`](@ref) function for this.
+=#
+a, b = 1, 0.1
+ccoeff = ComplexF64[0.5(a+b), 0, 0.5(a-b)]
+Zr = 0.0+0.0im
+α = π/4
+b = ConformalBody(ccoeff,Zr,α)
+plot(b)
+
+#=
+We can see how this mapping works by visualizing the gridlines in the circle ($\zeta$) plane
+and the corresponding lines in the physical plane (the $z$ plane). Importantly,
+all of the
+=#
+plot(b.m,size=(600,300))
+
+#=
+It is easy to get a flat plate from the ellipse by setting $b = 0$.
+
+More interesting shapes can be obtained by choosing the coefficients differently.
+There are a few rules on these coefficients:
+- $c_{1}$ must be present, and generally should be a positive real number. It
+  re-scales distances from the circle plane to the physical plane.
+- $c_{0}$ is usually zero.
+- The other coefficients are mostly free, but some combinations will break down.
+
+It is best to simply play around. Here is another one. Notice that
+we do not specify position or orientation here, so it defaults to the origin and
+zero angle.
+=#
+ccoeff = ComplexF64[1.0,0.0,0.0,0.1,0.1*exp(im*π/4)]
+b = ConformalBody(ccoeff)
+plot(b)
+
+
+#=
+### Polygonal bodies
+To create a polygonal body, we use a Schwarz-Christoffel conformal transformation,
+a general class of transformations that can map specific points on the
+circle to specified angles in the $z$ plane. That is done automatically by the
+`ConformalBody` function. We just need to provide it with a polygon.
+
+To set up a polygon, we specify the vertices in
+counterclockwise order, with coordinates provided in either (x,y) or complex notation.
+=#
+x = [-1.0,1.0,0.5,-0.5]
+y = [-1.0,-1.0,1.0,1.0]
+p = Polygon(x,y)
+b = ConformalBody(p)
+plot(b)
+
+#=
+This makes a simple trapezoid. Let's see the mapping more clearly
+=#
+plot(b.m,size=(600,300))
+
+#=
+How about a star?
+=#
+npts = 5
+dθ = 2π/(2npts)
+θ = collect(0:dθ:2π-dθ)
+w = (1 .+ 0.3cos.(npts*θ)).*exp.(im*θ) # the vertices
+p = Polygon(w)
+Zr = 0.0+0.0im
+α = dθ/2 # rotate to make one point of the star directed upward
+b = ConformalBody(p,Zr,α)
+plot(b)
+
+#=
+or an airfoil? Here, we use the NACA 4-digit series of airfoils, in
+which the first number specifies the camber, the second is the location
+of maximum camber, and the last two numbers the thickness. A NACA 4412
+has camber of 0.04 (relative to chord) located at 0.4 of the chord, and
+a thickness of 0.12. We implement this with a large
+number of vertices, so it looks smooth but actually it is a polygon!
+=#
+w = naca4(0.04,0.4,0.12;len=1)
+p = Polygon(w)
+b = ConformalBody(p)
+plot(b)
+
+#=
+Finally, a polygon need not have an enclosed area, but rather can double back
+on itself, to form a set of linked lines with zero thickness. The [`LinkedLines`](@ref)
+function allows us to specify the vertices of the line segments.
+=#
+x = -0.5:0.1:0.5
+p = LinkedLines(x,0.1*sin.(8*x))
+b = ConformalBody(p)
+plot(b)
+#=
+Let's see how the transform looks in this case:
+=#
+plot(b.m,size=(600,300))
---
+++ test/literate/2.5-FlowsPastConformalBodies.jl
@@ -0,0 +1,186 @@
+include("header.jl")
+
+#=
+# General potential flows around bodies in 2D (continued)
+Now that we have seen that conformal maps can generate interesting shapes,
+we would like to use them to solve potential flow problems.
+
+In complex notation, a planar potential flow is specified by the **complex potential**,
+which consolidates the scalar potential and streamfunction together:
+
+$$F(z) = \phi + \mathrm{i}\psi$$
+
+This, like the conformal transform $z(\zeta)$, is a *holomorphic* (or analytic)
+function, so it can be differentiated with respect to $z$. This derivative
+gives us the *complex velocity*, $w$.
+
+$$\dfrac{\mathrm{d}F}{\mathrm{d}z} = w(z) = u - \mathrm{i} v$$
+
+Note the minus sign, which is unavoidable.
+
+Remember that we are solving the problem in the circle plane, not the physical
+plane. Suppose that we have found the complex potential in the circle plane for our
+desired flow, $\hat{F}(\zeta)$. Then, the solution in the physical plane is
+**easily** obtained by a composite of the potential and the inverse conformal
+transform, $\zeta(z)$:
+
+$$F(z) = \hat{F}(\zeta(z))$$
+
+If we want the velocity in the physical plane, we just use the chain rule.
+
+$$w(z) = \dfrac{\mathrm{d}\hat{F}}{\mathrm{d}\zeta} \dfrac{\mathrm{d}\zeta}{\mathrm{d}z}$$
+
+Another consequence of using a conformal transform is that $\mathrm{d}\zeta/\mathrm{d}z = 1/z'(\zeta)$.
+So the velocity is just
+
+$$w(z) = \dfrac{1}{z'(\zeta)} \hat{w}(\zeta)$$
+
+(We have not explictly written it, but we should assume that each $\zeta$ on the right
+side is given by the inverse mapping, $\zeta(z)$.)
+=#
+
+using FluidDynTools
+#-
+using PotentialFlow
+using PotentialFlow.Bodies
+#-
+using Plots
+
+#=
+## Set up the body
+Let's choose an elliptical body to illustrate examples of the flow.
+We will also create a Cartesian grid of points to evaluate the streamfunction
+in the physical ($z$) plane. We also need to inverse-transform these grid points
+to the $\zeta$ plane, because each of the flow field solutions are obtained in the circle
+plane.
+=#
+a, b = 1, 0.1
+ccoeff = ComplexF64[0.5(a+b), 0, 0.5(a-b)]
+Zr = 0.0+0.0im
+α = -π/4
+b = ConformalBody(ccoeff,Zr,α)
+
+#=
+Set up the grid and transform each point to the $\zeta$ plane. This
+last step can be slow for some shapes.
+=#
+xg = range(-2,2,length=101)
+yg = range(-2,2,length=101)
+zg = [x + im*y for y in yg, x in xg]
+ζg = inverse_conftransform(zg,b);
+
+
+
+#=
+## Example 1: A point vortex near a stationary body
+Let's first look at a vortex placed near the body. We will specify
+body motion here, as well, but the motion will be zero.
+=#
+t = 0 # time is irrelevant here, but it is an argument in the function
+Żr = 0.0+0.0im # translational velocity
+α̇ = 0.0 # angular velocity
+motion = RigidBodyMotion(Żr, α̇)
+
+#=
+We place a vortex of strength 1 at a certain point. Before using it, we need to
+inverse-transform it to the circle plane
+=#
+zv = 0.7-0.2im
+Γv = 1.0
+vort_z = Vortex.Point(zv, Γv)
+sys = (vort_z,)
+sys = inverse_conftransform(sys,b)
+
+#=
+Now we account for the body's presence. This is done by the function [`enforce_no_flow_through!`](@ref),
+which solves the problem by creating an image inside the circle at $1/\zeta_{v}^{*}$
+of strength $-\Gamma_{v}$.
+(It does *not* create an image at the center of the circle, however. So the total
+circulation of this vortex and its image is zero.) Note that we use the `clear_images!`
+function to get rid of any images set up previously. There were none in this case,
+but it is a good habit.
+=#
+clear_images!(b)
+enforce_no_flow_through!(b, motion, sys, t)
+
+#=
+Now we are ready to plot the streamlines. There is a handy function [`streamlines`](@ref)
+that does this. It simply calls a function [`PotentialFlow.streamfunction`](@ref)
+and evaluates the streamfunction in the circle plane, using the points in `ζg`,
+and then plots the results on the corresponding Cartesian grid in the physical plane.
+=#
+ps = streamlines(xg,yg,ζg,(b,sys),levels=range(-0.5,0.5,length=31),xlim=(-2,2),ylim=(-2,2))
+plot!(ps,b)
+
+#=
+It is clear that the normally circular streamlines around the vortex have
+been deformed by the presence of the body, and no-flow-through
+is visually satisfied.
+
+## Example 2: Free stream past the body
+Now let's put the body in a free stream of speed 1. Like the vortex, we have
+to inverse-transform this free stream to the circle plane,
+and then call [`enforce_no_flow_through!`](@ref). This places an image
+in the body -- in this case, a doublet at the center of the circle.
+=#
+Winf = 1.0 + 0.0im
+fs = Freestreams.Freestream(conj(Winf))
+sys = (fs,)
+sys = inverse_conftransform(sys,b)
+
+clear_images!(b)
+enforce_no_flow_through!(b, motion, sys, t)
+
+ps = streamlines(xg,yg,ζg,(b,sys),levels=range(-2,2,length=31),xlim=(-2,2),ylim=(-2,2))
+plot!(ps,b)
+
+#=
+## Example 3: Body in translation
+In this example and the next, the body will be in motion in an otherwise
+quiescent flow. Let's make it move to the left with speed 1. This is the same
+problem we solved with the freestream, but now in a different reference frame.
+There are no other contributors to the problem, so the `sys` is blank.
+=#
+Żr = -1.0+0.0im # translational velocity
+α̇ = 0.0 # angular velocity
+motion = RigidBodyMotion(Żr, α̇)
+sys = ()
+
+clear_images!(b)
+enforce_no_flow_through!(b, motion, sys, t)
+ps = streamlines(xg,yg,ζg,(b,sys),levels=range(-2,2,length=31),xlim=(-2,2),ylim=(-2,2))
+plot!(ps,b)
+
+#=
+Notice how different these streamlines look compared to previous example. It is not obvious that
+they correspond to the same flow. It raises the important point that **streamlines
+depend on the frame of reference in which the flow is viewed**. In this
+reference frame, the flow is quiescent at infinity, and all of the motion
+is generated in the vicinity of the body. Notice how it curls back around the
+edges. That is characteristic of the streamlines due to body motion,
+because there must be some way for the fluid to "fill in" the region behind
+the moving body.
+=#
+
+#=
+## Now put them together
+Let's put a vortex near a stationary body in a uniform flow.
+=#
+Żr = 0.0+0.0im # translational velocity
+α̇ = 0.0 # angular velocity
+motion = RigidBodyMotion(Żr, α̇)
+
+Winf = 1.0
+fs = Freestreams.Freestream(conj(Winf))
+
+zv = 1.2-0.5im
+Γv = 2.5
+vort_z = Vortex.Point(zv, Γv)
+
+sys = (vort_z,fs)
+sys = inverse_conftransform(sys,b) # Inverse-transform them
+
+clear_images!(b)
+enforce_no_flow_through!(b, motion, sys, t)
+ps = streamlines(xg,yg,ζg,(b,sys),levels=range(-2,2,length=31),xlim=(-2,2),ylim=(-2,2))
+plot!(ps,b)
